b3c4fb2cfb5cad113f132ace10639f6eb408f2be
Shahriar <shahriaarrr@gmail.com>
2021-02-06 09:51:24 +0330

    develope UI file

--- src/UI.py
+++ src/UI.py
@@ -1,9 +1,25 @@
 from tkinter import *
 
-class main():
+
+
+class Main():
     def __init__(self):
         self.win = Tk()
+        # --- [config] ---- #
+        self.config_title = 'youtube downloader'
+        self.config_width = 350
+        self.confing_heigt = 250
+        self.config_resizable = [False, False]
+
+        # --- [load functions] --- #
+        self.Load_Configs()
+
+        mainloop()
+
+    def Load_Configs(self):
+        self.win.title(self.config_title)
+        self.win.geometry(str(self.config_width) + 'x' + str(self.confing_heigt))
+        self.win.resizable(self.config_resizable[0], self.config_resizable[1])
 
 
-
-mainloop()
+application = Main()
